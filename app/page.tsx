fad5e70d53765d515d73aa5760030a4a29610ac5
jvrtdevmac <joaovitorromanoteixeira@gmail.com>
2024-06-04 20:07:29 -0300

    :wrench: div vazia

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+'use client'
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MaxWidthWrapper>
-        teste
+        <div className="">
+          
+        </div>
       </MaxWidthWrapper>
     </main>
   );
