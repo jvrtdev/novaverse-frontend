3f111ce2b5a1e04c366e5c2ae20913de5e6cbd08
jvrtdev-linux <joaovitorromanoteixeira@gmail.com>
2024-06-04 23:58:28 -0300

    :wrench: feat: add map.products on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
 'use client'
+import { ProductProps } from "@/@types/product";
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+import Product from "@/components/Product";
+import { useCart } from "@/context/CartContext";
+import api from "@/services/api";
+import { fetchProducts } from "@/services/get-products";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [ products, setProducts ] = useState<ProductProps[]>([]);
+  const [ loading, setLoading ] = useState<boolean>(true)
+
+  useEffect(() => {
+    setLoading(true)
+    fetchProducts()
+    setLoading(false)
+  }, [])
+
+  const fetchProducts = async () => {
+    try{
+      const response = await api.get('/api/products')
+      return setProducts(response.data) 
+      
+    } catch(error) {
+      return error
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <MaxWidthWrapper>
-        <div className="">
-          
+        <div className="flex items-center justify-center">
+
+          <div className="flex gap-4">
+            {loading ? <div>CARREGANDO...</div> : ''}
+            {products.map((product) => (
+              <div>
+                <Product key={product.id} product={product} />
+              </div>
+            ))}  
+          </div>
         </div>
       </MaxWidthWrapper>
     </main>
